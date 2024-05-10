9d981905db9010f46a8811d336c504d0204f0090
himisterlee <lejlej100418@gmail.com>
2024-05-10 11:09:59 +0900

    출력 문자 변경

--- src/main/java/ohgiraffers/example.java
+++ src/main/java/ohgiraffers/example.java
@@ -4,6 +4,6 @@ public class example {
     public static void main(String[] args)  {
 
         System.out.print("Hello World!");
-        System.out.print("한글은 안 깨져요");
+        System.out.print("오늘은 금요일");
     }
 }
